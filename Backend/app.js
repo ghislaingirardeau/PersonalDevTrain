1c4339e7a79560ab5b97e9e8cba6fbb9a558a963
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-08 12:21:43 +0200

    config server

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+/* const usersRoute = require('./routes/users')
+const topicRoutes = require('./routes/topic') */
+
+const app = express()
+
+/* Configuration du header */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*') /* * pour la soutenance, pour la presentation mettre le 8080 */
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+  next()
+})    
+
+app.use(express.json())
+
+/* app.use('/api/compte', usersRoute)
+app.use('/api/sujet', topicRoutes) */
+
+module.exports = app
---
+++ Backend/config.js
@@ -0,0 +1,21 @@
+  
+/* CONNECTION MYSQL */
+
+/* Pour une connection en ligne à mysql exporter configWeb (par défaut) */
+let configWeb = {
+    host: 'freedb.tech',
+    port: 3306,
+    user: `${process.env.USER_DB}`,
+    password: `${process.env.PASSWORD_DB}`,
+    database: `${process.env.DATABASE_DB}`
+}
+
+/* Pour une connection en locale à mysql exporter config */
+let config = {
+    host: 'localhost',
+    user: `${process.env.USER}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DATABASE}`
+}
+
+module.exports = config; /* Changer ici le choix de la connection */

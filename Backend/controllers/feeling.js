e10dbf3224ee6182c395bbee022c799290ab4c79
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-06-08 16:37:55 +0200

    post et get of feeling pos and neg

--- Backend/controllers/feeling.js
+++ Backend/controllers/feeling.js
@@ -6,26 +6,44 @@ const connection = mysql.createConnection(config)
 
 exports.allFeeling = (req, res, next) => { 
     
-    const sql = `XXX`
+    const sql = `CALL all_feeling`
     connection.query(sql, (error, results, fields) => {
 
         if (error) {
-            res.status(400).json({})
+            res.status(400).json({message: "echec"})
         } else if (results) {
-            res.status(200).json({})
+            res.status(200).json({
+                positive: results[0],
+                totalPositif: results[0].length,
+                negative: results[1],
+                totalNegatif: results[1].length,
+            })
         }
     })
 }
 
-exports.createFeeling = (req, res, next) => { 
+exports.createnegative = (req, res, next) => { 
     
-    const sql = `XXX`
+    const sql = `CALL add_negative("${req.body.user_id}", "${req.body.feeling}")`
     connection.query(sql, (error, results, fields) => {
 
         if (error) {
-            res.status(400).json({})
+            res.status(400).json({message: 'echec de creation'})
         } else if (results) {
-            res.status(200).json({})
+            res.status(200).json({message: 'Impression envoyé'})
+        }
+    })
+}
+
+exports.createpositive = (req, res, next) => { 
+    
+    const sql = `CALL add_positive("${req.body.user_id}", "${req.body.feeling}")`
+    connection.query(sql, (error, results, fields) => {
+
+        if (error) {
+            res.status(400).json({message: 'echec de creation'})
+        } else if (results) {
+            res.status(200).json({message: 'Impression envoyé'})
         }
     })
 }
--- Backend/routes/feeling.js
+++ Backend/routes/feeling.js
@@ -5,7 +5,8 @@ const authentification = require('../middlewares/auth')
 const router = express.Router()
 
 router.get('/', authentification, feeling.allFeeling)
-router.post('/', authentification, feeling.createFeeling)  /* expect { "user_id":"XXX" ,"topic": "XXXX"} */
+router.post('/positive', authentification, feeling.createpositive)  /* expect { "user_id":"XXX" ,"feeling": "XXXX"} */
+router.post('/negative', authentification, feeling.createnegative)
 
 router.put('/:feeling_id', authentification, feeling.modifyFeeling)   /* send topicId IN URL PARAMS */
 router.delete('/:feeling_id', authentification, feeling.removeFeeling)   /* expect {"user_id":"XXX" ,"comment": "XXXX"} */

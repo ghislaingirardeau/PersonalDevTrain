67d115f25d191061579b8cb62042987cee87e238
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-23 11:28:49 +0200

    port server

--- Frontend/goodDeeds/nuxt.config.js
+++ Frontend/goodDeeds/nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  server: {
+    port: 8000 // default: 3000
+  },
+
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 

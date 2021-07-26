c7be7b871c3d901d04770f3508328a1bc51db641
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-26 15:28:21 +0200

    send pseudo and refactor object fetch

--- Frontend/goodDeeds/pages/home.vue
+++ Frontend/goodDeeds/pages/home.vue
@@ -1,6 +1,13 @@
 <template>
-<main align-items-center>
-  <NuxtLink to="/" @click.native="disconnect">Déconnexion</NuxtLink>
+<main class="container align-items-center" >
+  <header class="row">
+    <h3 class="col-4">Be to feel</h3>
+    <h3 class="col-4">Bienvenue {{pseudo}}</h3>
+    <nav class="col-8 ">
+      <NuxtLink to="/" @click.native="disconnect">Déconnexion</NuxtLink>
+    </nav>
+  </header>
+  
   <h1>Mon tableau de bord émotionnel</h1>
 
     <feelingManage />
@@ -20,42 +27,44 @@ export default {
         emotion: [],
         reload: false,
         dataLoad: false,
-        userFeel: {
-          user_id: Number
-        }
+        pseudo : String
       }
     },
     mounted () {
-        this.userFeel.user_id = sessionStorage.getItem('userId')
-        let token = sessionStorage.getItem('token')
-        fetch("http://localhost:3000/api/feeling/", {
-            method: "POST",
-            headers: {
-            "content-type": "application/json",
-            "Authorization" : 'Bearer ' + token
-            },
-            body: JSON.stringify(this.userFeel)
-        })
-        .then(response => {
-            if(response.ok) {
-                response.json()
-                .then(data => {
-                  /* Recupere toutes les emotions dans un tableau */
-                    data.negative.forEach(element => {
-                        this.emotion.push(element.feeling_neg)
-                    });
-                    data.positive.forEach(element => {
-                        this.emotion.push(element.feeling_pos)
-                    });
-                    this.dataLoad = true
-                })
-            } else { /* sinon j'envoie une erreur */
+      this.pseudo = sessionStorage.getItem('pseudo')
+
+      let user_id = sessionStorage.getItem('userId')
+      let token = sessionStorage.getItem('token')
+      const userFeel = {user_id: user_id}
+      fetch("http://localhost:3000/api/feeling/", {
+          method: "POST",
+          headers: {
+          "content-type": "application/json",
+          "Authorization" : 'Bearer ' + token
+          },
+          body: JSON.stringify(userFeel)
+      })
+      .then(response => {
+          if(response.ok) {
               response.json()
               .then(data => {
-                console.log(data) /* renvoie error du backend sur le frontend */
+                /* Recupere toutes les emotions dans un tableau */
+                console.log(data)
+                  data.negative.forEach(element => {
+                      this.emotion.push(element.feeling_neg)
+                  });
+                  data.positive.forEach(element => {
+                      this.emotion.push(element.feeling_pos)
+                  });
+                  this.dataLoad = true
               })
-            }
-        })
+          } else { /* sinon j'envoie une erreur */
+            response.json()
+            .then(data => {
+              console.log(data) /* renvoie error du backend sur le frontend */
+            })
+          }
+      })
     },
     methods: {
       disconnect() {

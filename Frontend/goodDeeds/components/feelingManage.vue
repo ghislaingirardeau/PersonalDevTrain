9d3ec29558cf23edf9e0f916614e38d2d0f96c48
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-28 12:06:35 +0200

    add font display

--- Frontend/goodDeeds/components/feelingManage.vue
+++ Frontend/goodDeeds/components/feelingManage.vue
@@ -1,12 +1,9 @@
 <template>
-        <aside class="col-4 align-items-center">
+        <aside class="col-4 align-items-center row aside__bloc">
 
-            <div class="col-12 text-center">
-                <h2 class="mb-5">Sélectionne une émotion :</h2>              
-            </div>
-            
+            <h2 class="col-12 text-center mb-3 aside__bloc--title">Sélectionne une émotion :</h2>              
 
-            <div class="col-12 text-center">
+            <div class="col-6 text-center">
 
                 <b-button v-b-modal.modal-1 class="mb-3">Positive</b-button>
 
@@ -22,7 +19,7 @@
                 </b-modal>
             </div>
 
-            <div class="col-12 text-center">
+            <div class="col-6 text-center">
                 
                 <b-button v-b-modal.modal-2 class="mb-3">Negative</b-button>
         
@@ -39,14 +36,14 @@
             </div>
 
             
-            <div class="text-center">
-                <p class="feeling__selected">{{feeling}}</p>
-                <button class="btn btn-primary" v-show="feeling" @click="postFeeling">Valider</button> 
+            <div class="col-12 text-center">
+                <p class="mb-4 feeling__selected">{{feeling}}</p>
+                <button class="btn btn-primary" v-if="feeling" @click="postFeeling">Valider</button> 
             </div>
 
-            <h2 class="mb-5">Légende, catégorie des émotions</h2>
+            <h2 class="mb-2 mt-3 aside__bloc--title">Légende, catégorie des émotions</h2>
 
-            <div class="d-flex legend" v-for="item in emotionsList.main" :key="item.feel">
+            <div class="d-flex align-items-center col-12 legend" v-for="item in emotionsList.main" :key="item.feel">
                 <svg wclassth="30" height="30" viewBox="595 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g>
                         <path  d="M611 2.00002C608.9 5.90002 608.6 14.4 610.4 19.7C613 27.7 621.7 35 629.9 36.2C631.9 36.4 632.5 35.9 633.6 33C638.5 19.5 629.5 3.50002 615.3 0.500021C612.7 2.08223e-05 611.9 0.300021 611 2.00002Z" :fill="item.colorPrimary"/>

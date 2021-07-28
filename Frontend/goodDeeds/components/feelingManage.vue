e4bbd257a5d4e8565855aa54701d1d0e9043d3a6
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-28 11:16:26 +0200

    add 3 leaves legend with variant colors

--- Frontend/goodDeeds/components/feelingManage.vue
+++ Frontend/goodDeeds/components/feelingManage.vue
@@ -2,10 +2,7 @@
         <aside class="col-4 align-items-center">
 
             <div class="col-12 text-center">
-                <h2 class="mb-5">Sélectionne une émotion :</h2> 
-
- 
-                
+                <h2 class="mb-5">Sélectionne une émotion :</h2>              
             </div>
             
 
@@ -36,7 +33,6 @@
                                 <input type="radio" :value="item" :id="item" name="emotion" v-model="feeling" @click="negativeSelect">
                                 <span class="checkmark"><fa icon="check" /></span>
                             </label>
-                          
                       </b-col>
                   </b-form-row>
                 </b-modal>
@@ -46,6 +42,27 @@
             <div class="text-center">
                 <p class="feeling__selected">{{feeling}}</p>
                 <button class="btn btn-primary" v-show="feeling" @click="postFeeling">Valider</button> 
+            </div>
+
+            <h2 class="mb-5">Légende, catégorie des émotions</h2>
+
+            <div class="d-flex legend" v-for="item in emotionsList.main" :key="item.feel">
+                <svg wclassth="30" height="30" viewBox="595 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path  d="M611 2.00002C608.9 5.90002 608.6 14.4 610.4 19.7C613 27.7 621.7 35 629.9 36.2C631.9 36.4 632.5 35.9 633.6 33C638.5 19.5 629.5 3.50002 615.3 0.500021C612.7 2.08223e-05 611.9 0.300021 611 2.00002Z" :fill="item.colorPrimary"/>
+                    </g>
+                </svg>
+                <svg wclassth="30" height="30" viewBox="595 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path  d="M611 2.00002C608.9 5.90002 608.6 14.4 610.4 19.7C613 27.7 621.7 35 629.9 36.2C631.9 36.4 632.5 35.9 633.6 33C638.5 19.5 629.5 3.50002 615.3 0.500021C612.7 2.08223e-05 611.9 0.300021 611 2.00002Z" :fill="item.colorSecondary"/>
+                    </g>
+                </svg>
+                <svg wclassth="30" height="30" viewBox="595 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path  d="M611 2.00002C608.9 5.90002 608.6 14.4 610.4 19.7C613 27.7 621.7 35 629.9 36.2C631.9 36.4 632.5 35.9 633.6 33C638.5 19.5 629.5 3.50002 615.3 0.500021C612.7 2.08223e-05 611.9 0.300021 611 2.00002Z" :fill="item.colorThird"/>
+                    </g>
+                </svg>
+                <p>{{item.feel}}</p>
             </div>
 
         </aside>
@@ -60,11 +77,11 @@ export default {
         return {
             feeling: undefined,
             emotionsList : emotions,
-            kindOfFeel: String /* envoie dans le bon tableau coté mysql */
+            kindOfFeel: String, /* envoie dans le bon tableau coté mysql */
         }
     },
     mounted() {
- 
+        
     },
     
     methods: {
@@ -100,6 +117,7 @@ export default {
                 /* envoie la nouvelle emotion dans le tableau arbre et reload le composant */
                 this.$parent.emotion.push(this.feeling)
                 this.$parent.reload = !this.$parent.reload
+
             } else {
                 console.log('selectionner une emotion')
             }

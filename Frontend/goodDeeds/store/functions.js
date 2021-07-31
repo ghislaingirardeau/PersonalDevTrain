4714bd0a61c0ea65490a7bf72f30547c41f1c796
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-31 18:04:38 +0200

    add third feeling

--- Frontend/goodDeeds/store/functions.js
+++ Frontend/goodDeeds/store/functions.js
@@ -1,6 +1,9 @@
+/* FUNCTION COLORS LEAVES ON MOUNTED */
+
 export function emotionsColor (emotion) {
     let index = 0 /* envoie l'index a l'objet */
     const object = document.getElementsByClassName(`Vector`)
+    let fear = ['scared', 'terrifier', 'insecure', 'nervous', 'horrfied']
     /* Ajout une class appear a chaque index tant que index est inferieur au nombre d'emotions */
         
     do {
@@ -9,8 +12,8 @@ export function emotionsColor (emotion) {
         /* envoie une couleur suivant emotion negative */
         
         switch (emotion[index]) {
-            
-            case 'fear' : object[index].setAttribute('fill', '#d12d17') /* red */
+            /*colors HSV: primary 98%, 82% \ secondary 69%, 87% \ third 39%, 92%*/
+            case 'fear' : object[index].setAttribute('fill', '#d11c04') /* red  */
             break;
             case 'scared':
             case 'terrifier':    
@@ -18,7 +21,7 @@ export function emotionsColor (emotion) {
             case 'nervous':
             case 'horrfied': object[index].setAttribute('fill', '#de5745')
             break;
-            case 'anger': object[index].setAttribute('fill', '#d18417') /* orange */
+            case 'anger': object[index].setAttribute('fill', '#d17c04') /* orange */
             break;
             case 'disgusted':
             case 'enraged':    
@@ -26,7 +29,7 @@ export function emotionsColor (emotion) {
             case 'exasperated':
             case 'jealous': object[index].setAttribute('fill', '#de9e45')
             break;
-            case 'sadness': object[index].setAttribute('fill', '#ebd61a') /* yellow */
+            case 'sadness': object[index].setAttribute('fill', '#d1bd04') /* yellow */
             break;
             case 'hurt':
             case 'unhappy':    
@@ -35,7 +38,7 @@ export function emotionsColor (emotion) {
             case 'gloomy':
             case 'lonely': object[index].setAttribute('fill', '#d1c341') 
             break;
-            case 'surprise': object[index].setAttribute('fill', '#171ad1') /* blue */
+            case 'surprise': object[index].setAttribute('fill', '#0408d1') /* blue */
             break;  
             case 'confused':
             case 'overcome':    
@@ -43,7 +46,7 @@ export function emotionsColor (emotion) {
             case 'amazed':
             case 'moved': object[index].setAttribute('fill', '#4548de') 
             break;
-            case 'joy': object[index].setAttribute('fill', '#46d117') /* green */
+            case 'joy': object[index].setAttribute('fill', '#37d104') /* green */
             break;
             case 'euphoric':
             case 'excited':    
@@ -54,25 +57,28 @@ export function emotionsColor (emotion) {
             case 'content':
             case 'happy': object[index].setAttribute('fill', '#6bde45')
             break;
-            case 'love': object[index].setAttribute('fill', '#17d1c5') /* turquoise hsv 89%, 82% */
+            case 'love': object[index].setAttribute('fill', '#04d1c3') /* turquoise */
             break;
             case 'sentimental':
             case 'affectionate':    
             case 'romantic':
             case 'enchanted':
-            case 'grateful': object[index].setAttribute('fill', '#45ded4') /* hsv 69%, 87% */
+            case 'grateful': object[index].setAttribute('fill', '#45ded4') 
         }
         
         index++
     } while(index < (emotion.length))
 }
 
+/* FUNCTION UPPERCASE USER NAME  */
+
 export function upperFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1) 
 }
 
-import axios from "axios"
+/*  FUNCTION AXIOS FOR CONNECTION LOGIN AND SIGNUP */
 
+import axios from "axios"
 export function postConnection (connectionType, post, errorMessage) {
     const data = JSON.stringify(post)
       axios.post("http://localhost:3000/api/" + connectionType, data, {
@@ -91,3 +97,4 @@ export function postConnection (connectionType, post, errorMessage) {
       })
       .catch((error) => errorMessage = error.response.data.message)
 }
+

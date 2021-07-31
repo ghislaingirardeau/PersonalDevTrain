f3555653441a89b3d011a2c77dc0f02acc5d76d3
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-07-31 12:19:15 +0200

    signup with axios

--- Frontend/goodDeeds/store/functions.js
+++ Frontend/goodDeeds/store/functions.js
@@ -70,3 +70,22 @@ export function emotionsColor (emotion) {
 export function upperFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1) 
 }
+
+export function postConnection (connectionType, post, errorMessage) {
+    const data = JSON.stringify(post)
+      axios.post("http://localhost:3000/api/" + connectionType, data, {
+        headers: {
+          "content-type": "application/json",
+        },
+      }) 
+      .then(response => {
+        
+        sessionStorage.setItem('token', response.data.token)
+        sessionStorage.setItem('userId', response.data.userId)
+        sessionStorage.setItem('pseudo', response.data.pseudo)
+        sessionStorage.setItem('role', response.data.role)
+        window.open('/home', '_self')
+
+      })
+      .catch((error) => errorMessage = error.response.data.message)
+}

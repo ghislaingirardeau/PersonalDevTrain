226909d584227644fc1811cfd923ff35cbde68e5
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-09-02 15:49:30 +0200

    refactor switch bloc watch checked

--- Frontend/goodDeeds/components/feelingManage.vue
+++ Frontend/goodDeeds/components/feelingManage.vue
@@ -3,25 +3,17 @@
 
         <div id="aside__selection" class="row aside__selection mt-3 mt-xl-0 mb-3 mb-xl-5 p-2 load">
             <h2 class="col-12 aside__bloc--title mb-4">Sélectionne une émotion</h2> 
-            <div class="col-12">
-                <b-form-checkbox v-model="checked" name="check-button" switch>
-                <p v-if="checked">Exhaustive liste</p>
-                <p v-else>Simple Liste</p>
-                </b-form-checkbox>
-            </div>            
             <div class="col-6">
                 <b-button v-b-modal.modal-positif class="mb-3 button__feel button__feel--colorlight">Positive</b-button>
                 <b-modal id="modal-positif" title="Liste émotions positives" size="lg">
-                  <b-form-row v-if="checked">
-                      <b-col cols="6" lg="3" v-for="item in emotionsList.positive" :key="item" >
-                          <label :for="item" class="label__display">{{upperFirstLetter(item)}}
-                                <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="positiveSelect">
-                                <span class="checkmark"><fa icon="check" /></span>
-                          </label>
-                      </b-col>
-                  </b-form-row>
-                  <b-form-row v-else>
-                      <b-col cols="6" lg="3" v-for="item in emotionsList.positiveSimple" :key="item" >
+                    <div class="col-12">
+                        <b-form-checkbox size="lg" v-model="checked" name="check-button" switch>
+                        <p class="switch__font--color" v-if="checked">Désactiver</p>
+                        <p class="switch__font--color" v-else>Activer la liste exhaustive</p>
+                        </b-form-checkbox>
+                    </div>            
+                  <b-form-row>
+                      <b-col cols="6" lg="3" v-for="item in listPositive" :key="item" >
                           <label :for="item" class="label__display">{{upperFirstLetter(item)}}
                                 <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="positiveSelect">
                                 <span class="checkmark"><fa icon="check" /></span>
@@ -33,16 +25,14 @@
             <div class="col-6">
                 <b-button v-b-modal.modal-negatif class="mb-3 button__feel button__feel--colordark" >Negative</b-button>
                 <b-modal id="modal-negatif" title="Liste émotions négatives" size="lg">
-                  <b-form-row v-if="checked">
-                      <b-col cols="6" lg="3" v-for="item in emotionsList.negative" :key="item" >
-                            <label class="label__display" :for="item">{{upperFirstLetter(item)}}
-                                <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="negativeSelect">
-                                <span class="checkmark"><fa icon="check" /></span>
-                            </label>
-                      </b-col>
-                  </b-form-row>
-                  <b-form-row v-else>
-                      <b-col cols="6" lg="3" v-for="item in emotionsList.negativeSimple" :key="item" >
+                    <div class="col-12">
+                        <b-form-checkbox size="lg" v-model="checked" name="check-button" switch>
+                        <p class="switch__font--color" v-if="checked">Désactiver</p>
+                        <p class="switch__font--color" v-else>Activer la liste exhaustive</p>
+                        </b-form-checkbox>
+                    </div>            
+                  <b-form-row>
+                      <b-col cols="6" lg="3" v-for="item in listNegative" :key="item" >
                             <label class="label__display" :for="item">{{upperFirstLetter(item)}}
                                 <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="negativeSelect">
                                 <span class="checkmark"><fa icon="check" /></span>
@@ -95,12 +85,14 @@ export default {
             feeling: undefined,
             emotionsList : emotions,
             kindOfFeel: String, /* envoie dans le bon tableau coté mysql */
-            checked: false
+            checked: false,
+            listPositive: emotions.positiveSimple,
+            listNegative: emotions.negativeSimple,
+            exhaustive: [],
         }
     },
     
     mounted() {
-        
         emotions.positive.sort(function (a, b) {
             return a.localeCompare(b);
         })
@@ -112,6 +104,17 @@ export default {
         revealAnimation()
         /* TIMER DISABLE VALIDATE EVERY 6H */
         timerSelectFeeling()
+    },
+    watch: {
+        checked: function(valeur) {
+            if(valeur === true){
+                this.listPositive = emotions.positive
+                this.listNegative = emotions.negative
+            } else {
+                this.listPositive = emotions.positiveSimple
+                this.listNegative = emotions.negativeSimple
+            }
+        }
     },
     
     methods: {

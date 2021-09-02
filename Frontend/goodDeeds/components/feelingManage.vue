720bc3fd50bd65f886b1b99411a9c1cc2c9aa4a0
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-09-02 13:41:58 +0200

    add option select emotion

--- Frontend/goodDeeds/components/feelingManage.vue
+++ Frontend/goodDeeds/components/feelingManage.vue
@@ -2,12 +2,26 @@
     <aside class="col-12 col-xl-4 text-center aside__bloc">
 
         <div id="aside__selection" class="row aside__selection mt-3 mt-xl-0 mb-3 mb-xl-5 p-2 load">
-            <h2 class="col-12 aside__bloc--title mb-4">Sélectionne une émotion</h2>              
+            <h2 class="col-12 aside__bloc--title mb-4">Sélectionne une émotion</h2> 
+            <div class="col-12">
+                <b-form-checkbox v-model="checked" name="check-button" switch>
+                <p v-if="checked">Exhaustive liste</p>
+                <p v-else>Simple Liste</p>
+                </b-form-checkbox>
+            </div>            
             <div class="col-6">
                 <b-button v-b-modal.modal-positif class="mb-3 button__feel button__feel--colorlight">Positive</b-button>
                 <b-modal id="modal-positif" title="Liste émotions positives" size="lg">
-                  <b-form-row>
+                  <b-form-row v-if="checked">
                       <b-col cols="6" lg="3" v-for="item in emotionsList.positive" :key="item" >
+                          <label :for="item" class="label__display">{{upperFirstLetter(item)}}
+                                <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="positiveSelect">
+                                <span class="checkmark"><fa icon="check" /></span>
+                          </label>
+                      </b-col>
+                  </b-form-row>
+                  <b-form-row v-else>
+                      <b-col cols="6" lg="3" v-for="item in emotionsList.positiveSimple" :key="item" >
                           <label :for="item" class="label__display">{{upperFirstLetter(item)}}
                                 <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="positiveSelect">
                                 <span class="checkmark"><fa icon="check" /></span>
@@ -19,8 +33,16 @@
             <div class="col-6">
                 <b-button v-b-modal.modal-negatif class="mb-3 button__feel button__feel--colordark" >Negative</b-button>
                 <b-modal id="modal-negatif" title="Liste émotions négatives" size="lg">
-                  <b-form-row>
+                  <b-form-row v-if="checked">
                       <b-col cols="6" lg="3" v-for="item in emotionsList.negative" :key="item" >
+                            <label class="label__display" :for="item">{{upperFirstLetter(item)}}
+                                <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="negativeSelect">
+                                <span class="checkmark"><fa icon="check" /></span>
+                            </label>
+                      </b-col>
+                  </b-form-row>
+                  <b-form-row v-else>
+                      <b-col cols="6" lg="3" v-for="item in emotionsList.negativeSimple" :key="item" >
                             <label class="label__display" :for="item">{{upperFirstLetter(item)}}
                                 <input type="radio" :value="upperFirstLetter(item)" :id="item" name="emotion" v-model="feeling" @click="negativeSelect">
                                 <span class="checkmark"><fa icon="check" /></span>
@@ -73,10 +95,12 @@ export default {
             feeling: undefined,
             emotionsList : emotions,
             kindOfFeel: String, /* envoie dans le bon tableau coté mysql */
+            checked: false
         }
     },
     
     mounted() {
+        
         emotions.positive.sort(function (a, b) {
             return a.localeCompare(b);
         })
@@ -104,7 +128,7 @@ export default {
         cancelFeeling() {
             this.feeling = undefined
         },
-        upperFirstLetter
+        upperFirstLetter,
     },
 }
 </script>

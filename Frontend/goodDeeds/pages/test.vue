fbac105adcefbbfafcf6f3c414f23c38be7c7176
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-09-14 12:44:57 +0200

    set test audio

--- Frontend/goodDeeds/pages/test.vue
+++ Frontend/goodDeeds/pages/test.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
     <h1>test audio</h1>
-    <button @click="testAudio" :key="mute">Oscillateur</button>
-    <button @click="micro">test audio</button>
-    <button @click="changeText">{{isPlaying ? 'Play' : 'Stop'}}</button>
+    <label for="">frequence</label>
+    <input type="range" min="0" max ="1600" step="100" v-model="frequence" @change="testAudio"> {{frequence}}
+    <button @click="testAudio">{{!mute ? 'Play' : 'Stop'}}</button>
+    <button @click="micro">{{!mute ? 'Start' : 'Stop'}}</button>
+    <p>{{frequenceResults}}</p>
     </div>
 </template>
 
@@ -12,67 +14,80 @@ export default {
     data() {
         return {
             mute: false,
-            frequence: 100,
-            isPlaying: false
+            frequence: 440,
+            oscillator: 0,
+            source: 0,
+            analyser: 0,
+            audioCtx: 0,
+            interval: 0,
+            fftData: 0,
+        }
+    },
+    computed: {
+        frequenceResults() {
+            if(this.fftData < -125) {
+                    return "ok" + this.fftData
+                } else {
+                    return "mauvaise"
+                }
         }
     },
     methods: {
         testAudio() { /* creer un son a partir d'un oscillateur */
             this.mute = !this.mute
-            var volume
-            var analyser;
+            var vm = this
             
-            this.frequence = this.frequence + 100
-            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-            
-            // create Oscillator node
-            var oscillator = audioCtx.createOscillator();
-            analyser = audioCtx.createAnalyser();
-
-            var gainNode = audioCtx.createGain();
+            var gainNode
             if(this.mute == false) {
-                volume = 0
+                vm.oscillator.stop();
+                clearInterval(vm.interval)
             } else {
-                volume = 1
-            }
-            console.log(volume)
-            gainNode.gain.value = volume
-            console.log(gainNode.gain)
+                var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                vm.analyser = audioCtx.createAnalyser();
+                vm.oscillator = audioCtx.createOscillator();
+                gainNode = audioCtx.createGain();
+                vm.oscillator.type = 'square';
+                vm.oscillator.frequency.setValueAtTime(vm.frequence, audioCtx.currentTime); // value in hertz
+                vm.oscillator.connect(vm.analyser);
+                vm.analyser.connect(gainNode);
+                gainNode.connect(audioCtx.destination);
+                vm.oscillator.start();
+                vm.interval = setInterval(function(){
+                        var FFTData = new Float32Array(1024);
+                        vm.analyser.getFloatFrequencyData(FFTData);
+                        vm.fftData = FFTData[0]
+                    },2000);
 
-            oscillator.type = 'square';
-            oscillator.frequency.setValueAtTime(600, audioCtx.currentTime); // value in hertz
-            oscillator.connect(gainNode);
-            gainNode.connect(analyser);
-            analyser.connect(audioCtx.destination);
-            oscillator.start();
-                setInterval(function(){
-                    var FFTData = new Float32Array(analyser.frequencyBinCount);
-                    analyser.getFloatFrequencyData(FFTData);
-                    console.log(FFTData[0]);
-                },1000);
+            }
+
 
         },
         micro() {
             this.mute = !this.mute
-            var analyser;
-            navigator.mediaDevices.getUserMedia ({audio: true})
-            .then(function (stream) {
-                var audioCtx = new AudioContext(); /* new context audio */
-                analyser = audioCtx.createAnalyser(); /* creation de l'analyseur de frequence */
-                var source = audioCtx.createMediaStreamSource(stream); /* je créée un nouveau stream */
-                source.connect(analyser);
-                analyser.connect(audioCtx.destination) /* Envoie le stream dans le haut parleur */
-                setInterval(function(){
-                    var FFTData = new Float32Array(analyser.frequencyBinCount);
-                    analyser.getFloatFrequencyData(FFTData);
-                    console.log(FFTData[0]);
-                },1000);
-            })
-        },
-        changeText() {
             var vm = this
-            vm.isPlaying = !vm.isPlaying
-        }
+
+            if (this.mute == false) {
+                /* vm.source.disconnect(vm.analyser);
+                vm.analyser.disconnect(vm.audioCtx.destination);
+                vm.audioCtx.close() */
+            } else {
+                navigator.mediaDevices.getUserMedia ({audio: true})
+                .then(function (stream) {
+                vm.audioCtx = new AudioContext(); /* new context audio */
+                vm.analyser = vm.audioCtx.createAnalyser(); /* creation de l'analyseur de frequence */
+                vm.source = vm.audioCtx.createMediaStreamSource(stream); /* je créée un nouveau stream */
+                vm.source.connect(vm.analyser);
+                vm.analyser.connect(vm.audioCtx.destination) /* Envoie le stream dans le haut parleur */
+                    setInterval(function(){
+                        var FFTData = new Float32Array(1024);
+                        vm.analyser.getFloatFrequencyData(FFTData);
+                        console.log(vm.analyser.fftSize);
+                    },1000);
+                })
+
+            }
+            
+        },
     }
 }
 </script>
